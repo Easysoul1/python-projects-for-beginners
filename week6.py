9eccb3f9dae5c37c65caf0be7284e69f85e254fb
Adedayo Ezekiel Adejumo <167407110+Easysoul1@users.noreply.github.com>
2025-01-19 00:37:33 +0100

    Update week6.py

--- week6.py
+++ week6.py
@@ -2,24 +2,24 @@
 # Question 1 Week 6
 # Prime number checker
 # A prime number is a number that is only divisible by 1 and itself. For example, 2, 3, 5, 7, 11, 13, 17, etc. 
-# import math
-# range_check = int(input('Enter the maximun number for the prime check: '))
-# for i in range(2, range_check):
-#               is_primeNumber = True
-#               for j in range(2, int(math.sqrt(i))+1):
-#                      if i % j == 0:
-#                             is_primeNumber = False
-#                             break
-#               else:
-#                      print(f'{i} is a prime number')
+import math
+range_check = int(input('Enter the maximun number for the prime check: '))
+for i in range(2, range_check):
+              is_primeNumber = True
+              for j in range(2, int(math.sqrt(i))+1):
+                     if i % j == 0:
+                            is_primeNumber = False
+                            break
+              else:
+                     print(f'{i} is a prime number')
 
 
 # Question 2
 # Multiplication Table
 print('This is mulyiplication table 1 - 12')
-# for i in range(1, 13):
-#        for j in range(1, 13):
-#               print(f'{i} * {j} = {i * j}\n')
+for i in range(1, 13):
+       for j in range(1, 13):
+              print(f'{i} * {j} = {i * j}\n')
 
 
 # Question 3
