45f04c71d0ebfe498ba967abbd9d2fff89d9a1d3
Adedayo Ezekiel Adejumo <167407110+Easysoul1@users.noreply.github.com>
2025-01-19 00:38:36 +0100

    Update week8.py

--- week8.py
+++ week8.py
@@ -1,32 +1,32 @@
 # Pratice Exercises
 # Question 1 Week 8
 # Factorial calculation using recursive function
-# def factorial(number):
-#        if number == 0 or number == 1:
-#               return 1
-#        elif number < 0:
-#               return "Enter a valid input greater than 1 !"
-#        else:
-#               return number * factorial(number - 1)
+def factorial(number):
+       if number == 0 or number == 1:
+              return 1
+       elif number < 0:
+              return "Enter a valid input greater than 1 !"
+       else:
+              return number * factorial(number - 1)
 
-# print(factorial(5))
-# print(factorial(-5))
-# print(factorial(1))
+print(factorial(5))
+print(factorial(-5))
+print(factorial(1))
 
 # Question 2
 # Function calls using decorator
-# def main(func):
-#        def wrapper(*args, **kwargs):
-#               result = func(*args, **kwargs)
-#               print(f'The result is : {result}')
-#               return result
-#        return wrapper
+def main(func):
+       def wrapper(*args, **kwargs):
+              result = func(*args, **kwargs)
+              print(f'The result is : {result}')
+              return result
+       return wrapper
 
-# @main
-# def add(a, b):
-#        return a + b
+@main
+def add(a, b):
+       return a + b
 
-# add(5, 7)
+add(5, 7)
 
 # Question 3
 # Algorithm for Tower of Hanoi 
